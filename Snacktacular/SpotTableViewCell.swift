325310acb7b58ac0a26d766e5307d91b70eaa188
Andrew Chapman <andrew.chapman@bc.edu>
2021-10-31 19:53:32 -0400

    8.3 Initial User interface

---
+++ Snacktacular/SpotTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  SpotTableViewCell.swift
+//  Snacktacular
+//
+//  Created by Andrew Chapman on 10/31/21.
+//
+
+import UIKit
+
+class SpotTableViewCell: UITableViewCell {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    
+
+}
